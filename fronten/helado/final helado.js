75f0c46a5253f48277664890b19c1c43a80ca12e
Walter M. Aliser <wmaliser4@gmail.com>
2023-01-11 20:42:57 -0300

    mejoras en el js, no esta siendo facil, consegui agregar un div, dentro del div parrafos, pero no estoy consiguiendo poner una imagen

--- fronten/helado/final helado.js
+++ fronten/helado/final helado.js
@@ -8,10 +8,46 @@ function toggleproductDetail() {
     productDetail.classList.toggle('inactive');
 }
 
+
+/*Aqui empeze a practicar como insertar parrafos en js 11/012023*/
+
+/*
+<div class="carts_embutido">
+           <div class="text_embutido">
+              <p class="p1_embutido">Jamon Cosido</p>
+              <img class="logo_embutido" src="../fiambre/img-fiambre/logo champion.svg" alt="logo champion">
+              <p class="precio1_embutido">$ <span class="precio2_embutido">190</span>.00</p>
+              <p class="p2_embutido">100 Gr</p>
+              <a href="./" class="pcompra_embutido">Comprar</a>
+           </div>
+           <img class="img_embutido" src="../fiambre/img-fiambre/salami-sandwich-open-sandwich-of-salami-slices-on-bread.jpg" alt="jamon">
+        </div>
+*/
+const elementoPadre = document.querySelector('.padre');
+
+
+
+const div1 = document.createElement('div');
+
 const parrafoUno = document.createElement('p');
-const textoParrafoUno = document.createTextNode('Parrafo 1');
+const textoParrafoUno = document.createTextNode('Jamon Cosido');
 
-const elementoPadre = document.querySelector('.padre')
+const logoCards = document.createElement('img')
 
-elementoPadre.appendChild(parrafoUno);
-parrafoUno.appendChild(textoParrafoUno)
+
+const parrafoDos = document.createElement('p');
+const textoParrafoDos = document.createTextNode('$' + '190');
+
+const parrafoTres = document.createElement('p');
+const textoParrafoTres = document.createTextNode('100 Gr');
+
+
+div1.appendChild(parrafoUno);
+div1.appendChild(logoCards);
+div1.appendChild(parrafoDos);
+div1.appendChild(parrafoTres);
+
+elementoPadre.appendChild(div1);
+parrafoUno.appendChild(textoParrafoUno);
+parrafoDos.appendChild(textoParrafoDos);
+parrafoTres.appendChild(textoParrafoTres);
